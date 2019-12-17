b57a98d24b99e2af3f2315647a5632bbd4441efa
Aliaksandr Shtokin <stormboxvi@gmail.com>
2019-12-17 14:08:38 +0300

    Minor fix

--- resources/js/reducers/birthdaysReducer.js
+++ resources/js/reducers/birthdaysReducer.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_BIRTHDAYS:
-      return { ...state, ..._.mapKeys(action.payload, 'id') };
+      return { ...state, ...action.payload};
     case FETCH_BIRTHDAY:
     case CREATE_BIRTHDAY:
     case UPDATE_BIRTHDAY:
